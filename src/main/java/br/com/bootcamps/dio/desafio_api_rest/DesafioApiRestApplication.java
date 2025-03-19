01622759d2ae6cb4e58bbad95d06e5c469fb18b5
Priscilla Melo <priscillamelo73@gmail.com>
2025-03-19 09:09:32 -0300

    Initial commit

---
+++ src/main/java/br/com/bootcamps/dio/desafio_api_rest/DesafioApiRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.bootcamps.dio.desafio_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bootcamps/dio/desafio_api_rest/DesafioApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bootcamps.dio.desafio_api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

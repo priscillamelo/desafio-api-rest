22d85aa665b9246f0316ad0ff5e830344e4fa4c4
Priscilla Melo <priscillamelo73@gmail.com>
2025-03-19 11:00:38 -0300

    feat: create repositories

---
+++ src/main/java/br/com/bootcamps/dio/desafio_api_rest/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.bootcamps.dio.desafio_api_rest.repository;
+
+import br.com.bootcamps.dio.desafio_api_rest.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
